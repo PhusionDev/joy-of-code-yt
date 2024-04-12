c9c6b91442aeb15f22a48ad3213bd285207aa02a
Jared Hodgkins <jared.hodgkins@gmail.com>
2024-04-12 12:18:13 -0400

    2:29:41 Your Layout Files Can Also Load Data (complete)

---
+++ joc-loading-data/src/routes/posts/+page.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+
+	$: ({ posts } = data);
+</script>
+
+<h1>Posts</h1>
+
+<p>Showing {data.posts.length} posts.</p>
+
+{#if data.posts.length > 0}
+	<ul>
+		{#each posts as { slug, title }, i}
+			<li><a href="/posts/{slug}">{title}</a></li>
+		{/each}
+	</ul>
+{/if}
